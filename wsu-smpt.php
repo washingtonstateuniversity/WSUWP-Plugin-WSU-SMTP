cd193ce26901273829e12e322b3f3c180acaeaff
jeremyfelt <jeremy.felt@gmail.com>
2014-04-11 11:41:07 -0700

    Only check the current site when in multisite
    Corrects fatal error for undefined function otherwise

--- wsu-smpt.php
+++ wsu-smpt.php
@@ -14,7 +14,13 @@ add_action( 'phpmailer_init', 'wsuwp_smtp_email' );
 function wsuwp_smtp_email( $phpmailer ) {
 	$phpmailer->Mailer = 'smtp';
 	$phpmailer->From = sanitize_email( 'www-data@' . $_SERVER['SERVER_NAME'] );
-	$phpmailer->FromName =  esc_html( get_option( 'blogname' ) ) . ' | ' . esc_html( get_current_site()->site_name );
+
+	if ( is_multisite() ) {
+		$phpmailer->FromName = esc_html( get_option( 'blogname' ) ) . ' | ' . esc_html( get_current_site()->site_name );
+	} else {
+		$phpmailer->FromName = esc_html( get_option( 'blogname' ) );
+	}
+
 	$phpmailer->Sender = $phpmailer->From;
 	$phpmailer->Host = 'smtp.wsu.edu';
 	$phpmailer->Port = 25;
