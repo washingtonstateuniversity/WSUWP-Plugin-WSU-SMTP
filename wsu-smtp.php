0730f3d3d82e516e7048e846a130fa99ad8bb1f2
jeremyfelt <jeremy.felt@gmail.com>
2016-03-01 14:39:55 -0800

    Only overwrite FROM email on admin notifications

--- wsu-smtp.php
+++ wsu-smtp.php
@@ -15,6 +15,14 @@ add_action( 'phpmailer_init', 'wsuwp_smtp_email' );
  */
 function wsuwp_smtp_email( $phpmailer ) {
 	$phpmailer->Mailer = 'smtp';
+	$phpmailer->Host = 'smtp.wsu.edu';
+	$phpmailer->Port = 25;
+	$phpmailer->SMTPAuth = false;
+
+	if ( ! is_admin() ) {
+		return;
+	}
+
 	$phpmailer->From = sanitize_email( 'www-data@' . $_SERVER['SERVER_NAME'] );
 
 	if ( is_multisite() ) {
@@ -24,7 +32,4 @@ function wsuwp_smtp_email( $phpmailer ) {
 	}
 
 	$phpmailer->Sender = $phpmailer->From;
-	$phpmailer->Host = 'smtp.wsu.edu';
-	$phpmailer->Port = 25;
-	$phpmailer->SMTPAuth = false;
 }
